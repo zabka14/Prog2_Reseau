3fdf3a5ec874aeb4e2a450828ea65a725213e488
zabka14 <vianey.benjamin@gmail.com>
2015-11-02 16:03:16 +0100

    Création serveur TCP basique sur port 3333

--- src/application/app.java
+++ src/application/app.java
@@ -1,5 +1,10 @@
 package application;
 
+/**
+ * 
+ * @author Bodet Cindy - Vianey Benjamin
+ * Cette classe est le point d'entrée de l'application : elle lit un fichier de configuration et lance l'implementation adequate
+ */
 public class app {
 
 	public static void main(String[] args) {
